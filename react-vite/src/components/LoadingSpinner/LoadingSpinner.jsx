b8d443b62d6a62cb180a285adbb2f9aea74bdfa0
erik <erikhervall11@gmail.com>
2024-07-13 14:06:55 -0700

    fix

--- react-vite/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ react-vite/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -4,7 +4,6 @@ const LoadingSpinner = () => {
   return (
     <div className="loading-container">
       <img src="/shakerr.png" alt="Loading..." className="shaker" />
-      {/* <p>Shaking...</p> */}
     </div>
   );
 };
